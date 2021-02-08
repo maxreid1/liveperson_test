99da01145aa690ef48b42d92ee5434f2c5d68399
maxreid1 <max.reid@looker.com>
2021-02-08 18:09:27 -0500

    first_upload
    test

---
+++ custom_messaging_example.mjs
@@ -0,0 +1,6 @@
+looker_global_messages([
+  {
+    message: "Please note: all schedules must be set between the hours of 6-10AM UTC, and cannot be set more frequently than daily. All schedules outside that range will be moved and you will be notified.",
+    "level": "notice",
+  }
+])
